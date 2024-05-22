48cb4a58f9aa7b6fb83169c7ea81b1414c3e9393
Gabyfidelibus <gabrielfidelibus@gmail.com>
2024-05-21 23:40:46 -0300

    añadiendo addEventListener

--- js/main.js
+++ js/main.js
@@ -1,16 +1,6 @@
 let contenedor_productos = document.querySelector(".contenedor-productos");
 let contenedor_carrito = document.querySelector(".contenedor-carrito");
 
-function agregarProductos(producto) {
-    productoHTML = document.createElement("div");
-    productoHTML.classList.add("producto");
-    productoHTML.setAttribute("id",producto.id);
-    productoHTML.innerHTML =   `<h4>Producto: ${producto.nombre}</h4>
-                                <b>Precio en pesos: $${producto.precio}</b>
-                                <p>Cantidad diponible: ${producto.stock}</p>`;
-    contenedor_productos.appendChild(productoHTML);
-}
-
 let carrito = [];
 
 const productos =  [{id: "p01", nombre: "iPhone 13", precio: 1000000, stock: 2},
@@ -20,41 +10,100 @@ const productos =  [{id: "p01", nombre: "iPhone 13", precio: 1000000, stock: 2},
                     {id: "p05", nombre: "iPhone 15 pro", precio: 2500000, stock: 6},
                     {id: "p06", nombre: "iPhone 15 pro max", precio: 2800000, stock: 1}];
 
-productos.forEach(producto => {
-    agregarProductos(producto);
-});
+function eliminarDelCarrito(entrada) {
+    let indiceCarrito = carrito.findIndex(producto => producto.nombre.toLowerCase() === entrada);
+    let indiceProductos = productos.findIndex(producto => producto.nombre.toLowerCase() === entrada);
+    if (carrito[indiceCarrito].cantidad === 1){
+        carrito.splice(indiceCarrito,1);
+        productos[indiceProductos].stock++;
+    } else {
+        carrito[indiceCarrito].cantidad--;
+        productos[indiceProductos].stock++;
+    }
+    mostrarCarrito(carrito);
+    mostrarProductos(productos);
+}
 
-function agregarAlCarrito(carrito, productos) {
-    let entrada = prompt("Agrega un producto al carrito (escribe su nombre o 'salir' si no quiere agregar mas productos)").toLowerCase();
-    while (entrada != "salir") {
-        indice = productos.findIndex(producto => producto.nombre.toLowerCase() === entrada);
-        if (indice === -1) {
-            console.log("Producto no disponible");
-        } else {
-            let cantidad = Number(prompt("Ingrese la cantidad que desea agregar al carrito"));
-            while (cantidad <= 0 || cantidad > productos[indice].stock) {
-                console.log(`Ingrese un numero entre 1 y ${productos[indice].stock}`);
-                cantidad = Number(prompt("Ingrese la cantidad que desea agregar al carrito"));
-            }
-            indiceCarrito = carrito.findIndex(producto => producto.nombre.toLowerCase() === entrada);
+function mostrarCarrito(carrito) {
+
+    let elementos = contenedor_carrito.querySelectorAll(".producto");
+    elementos.forEach(element => {
+        element.remove();
+    });
+    let total = contenedor_carrito.querySelector(".total");
+    if (total) {
+        total.remove();
+    }
+
+    carrito.forEach(producto => {
+        let botonEliminar = document.createElement("button");
+        botonEliminar.innerText = '❌';
+        botonEliminar.addEventListener("click", () => {eliminarDelCarrito(producto.nombre.toLowerCase())});
+
+        let productoHTML = document.createElement("div");
+        productoHTML.classList.add("producto");
+        productoHTML.innerHTML =   `<h4>${producto.nombre}</h4>
+                                    <b>$${producto.precio}</b>
+                                    <p>Cantidad: ${producto.cantidad}</p>`;
+        
+        productoHTML.appendChild(botonEliminar);
+        contenedor_carrito.appendChild(productoHTML);
+    });
+
+    if (carrito.length > 0){
+        let totalHTML = document.createElement("h4");
+        totalHTML.classList.add("total");
+        totalHTML.innerText = `TOTAL: $${sumarTotal(carrito)}`;
+        contenedor_carrito.appendChild(totalHTML);
+    }
+}
+
+function agregarAlCarrito(entrada) {
+    let indice = productos.findIndex(producto => producto.nombre.toLowerCase() === entrada);
+    if (productos[indice].stock >= 1){
+        let cantidad = Number(prompt("Ingrese la cantidad que desea agregar al carrito"));
+        while (cantidad < 0 || cantidad > productos[indice].stock) {
+            console.log(`Ingrese un numero entre 1 y ${productos[indice].stock}`);
+            cantidad = Number(prompt("Ingrese la cantidad que desea agregar al carrito"));
+        }
+        if (cantidad > 0) {
+            let indiceCarrito = carrito.findIndex(producto => producto.nombre.toLowerCase() === entrada);
             productos[indice].stock -= cantidad;
             if (indiceCarrito === -1) {
                 carrito.push({nombre: productos[indice].nombre, precio: productos[indice].precio, cantidad: cantidad});
             } else {
                 carrito[indiceCarrito].cantidad += cantidad;
             }
+            mostrarCarrito(carrito);
+            mostrarProductos(productos);
         }
-        entrada = prompt("Agrega un producto al carrito (escribe su nombre o 'salir' si no quiere agregar mas productos)").toLowerCase();
+    } else {
+        console.log("No hay stock de este producto.")
     }
+    
 }
 
-function mostrarCarrito(producto) {
-    productoHTML = document.createElement("div");
-    productoHTML.classList.add("producto");
-    productoHTML.innerHTML =   `<h4>Producto: ${producto.nombre}</h4>
-                                <b>Precio en pesos: $${producto.precio}</b>
-                                <p>Cantidad: ${producto.cantidad}</p>`;
-    contenedor_carrito.appendChild(productoHTML);
+function mostrarProductos(productos) {
+
+    let elementos = contenedor_productos.querySelectorAll(".producto");
+    elementos.forEach(element => {
+        element.remove();
+    });
+
+    productos.forEach(producto => {
+        let botonAgregar = document.createElement("button");
+        botonAgregar.innerText = '➕';
+        botonAgregar.addEventListener("click", () => {agregarAlCarrito(producto.nombre.toLowerCase())});
+
+        let productoHTML = document.createElement("div");
+        productoHTML.classList.add("producto");
+        productoHTML.setAttribute("id",producto.id);
+        productoHTML.innerHTML =   `<h4>${producto.nombre}</h4>
+                                    <b>$${producto.precio}</b>
+                                    <p>Cantidad diponible: ${producto.stock}</p>`;
+        productoHTML.appendChild(botonAgregar);
+        contenedor_productos.appendChild(productoHTML);
+    });
 }
 
 function sumarTotal(carrito) {
@@ -63,44 +112,4 @@ function sumarTotal(carrito) {
     return total;
 }
 
-setTimeout(() => {
-    agregarAlCarrito(carrito, productos);
-
-    carrito.forEach(producto => {
-        mostrarCarrito(producto);
-    });
-    totalHTML = document.createElement("h4");
-    totalHTML.innerText = `TOTAL $${sumarTotal(carrito)}`;
-    contenedor_carrito.appendChild(totalHTML);
-}, 200);
-
-// Funcion tipo cambiario
-/*
-function conversionMoneda(precio, tipo){
-    let resultado = 0;
-    switch (tipo) {
-        case 'dolar':
-            resultado = 'US$ ' + (precio / 1040).toFixed(2);
-            break;
-        case 'euro':
-            resultado = '€ ' + (precio / 1085).toFixed(2);
-            break;
-        case 'libras':
-            resultado = '£ ' + (precio / 1286).toFixed(2);
-            break;
-        case 'yuan':
-            resultado = 'CN¥ ' + (precio / 121.3).toFixed(2);
-            break;
-        case 'yen':
-            resultado = 'JN¥ ' + (precio / 5.74).toFixed(2);
-            break;
-        default:
-            resultado = '$ ' + precio.toFixed(2);
-    }
-    return resultado;
-}
-
-let precio = Number(prompt("Ingresar el precio en pesos"));
-let moneda = prompt("Indicar la moneda deseada (dolar, euro, libras, yuan o yen)").toLowerCase();
-console.log('El precio es de ' + conversionMoneda(precio,moneda));
-*/
+mostrarProductos(productos);
